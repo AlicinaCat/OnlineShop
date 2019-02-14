1e29073e5f865d73d1555f3832c60a98f460e471
alicinacat <alice.casaburi@gmail.com>
2019-02-14 15:13:51 +0100

    Added models via entity framework

--- Online-Shop/Controllers/HomeController.cs
+++ Online-Shop/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Online_Shop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,10 +13,19 @@ namespace Online_Shop.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private TomasosContext _context;
+
+        public HomeController(TomasosContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var model = _context.Food.ToList();
+
+            return View(model);
         }
     }
 }
--- Online-Shop/Startup.cs
+++ Online-Shop/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Online_Shop.ModelsIdentity;
 using Microsoft.Extensions.Configuration;
+using Online_Shop.Models;
 
 namespace Online_Shop
 {
@@ -34,6 +35,9 @@ namespace Online_Shop
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            services.AddDbContext<TomasosContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
