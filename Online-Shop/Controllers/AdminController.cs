0c5aa67978d3b98b69595eaa3d03c4656fcd3611
alicinacat <alice.casaburi@gmail.com>
2019-02-17 17:57:24 +0100

    Started working on the admin section and the managing options

--- Online-Shop/Controllers/AdminController.cs
+++ Online-Shop/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Online_Shop.Models;
+using Online_Shop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +14,62 @@ namespace Online_Shop.Controllers
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        private TomasosContext _context;
+
+        public AdminController(TomasosContext context)
+        {
+            _context = context;
+        }
 
         // GET: /<controller>/
         public IActionResult Index()
         {
+
             return View();
+        }
+
+        public IActionResult ManageMenu()
+        {
+            var model = GetViewModel();
+
+            return View(model);
+        }
+
+        public IActionResult EditMenu(int id)
+        {
+            var model = GetViewModel();
+
+            model.CurrentFood = model.AllFoods.SingleOrDefault(f => f.FoodId == id);
+
+            return View(model);
+        }
+
+        public IActionResult ManageOrders()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public ViewModelFood GetViewModel()
+        {
+            ViewModelFood model = new ViewModelFood();
+            model.AllFoods = _context.Food.ToList();
+            model.AllFoodIngredients = _context.FoodIngredient.ToList();
+            model.AllIngredients = _context.Ingredient.ToList();
+
+            foreach (var item in model.AllFoods)
+            {
+                foreach (var ing in model.AllFoodIngredients.Where(f => f.FoodId == item.FoodId).ToList())
+                {
+                    item.Ingredients.Add(model.AllIngredients.SingleOrDefault(i => i.IngredientId == ing.IngredientId));
+                }
+            }
+
+            return model;
         }
     }
 }
