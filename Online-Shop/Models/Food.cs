01eeb6b3ddeb5df59bd0a6509f0f26b8f362763c
AlicinaCat <alice.casaburi@gmail.com>
2019-02-22 12:35:39 +0100

    Fixed some stying

--- Online-Shop/Models/Food.cs
+++ Online-Shop/Models/Food.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Online_Shop.Models
@@ -15,6 +16,8 @@ namespace Online_Shop.Models
         }
 
         public int FoodId { get; set; }
+
+        [Required(ErrorMessage = "Please fill in the username.")]
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
